348e8a02ac8509e071c1efafeee2b0dd2c2e14ec
zraso <zaira.rasool@hotmail.co.uk>
2019-12-10 13:05:51 +0000

    Curl post request responds with json in correct format

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
+require 'json'
 
 class UrlShortener < Sinatra::Base 
 
   get '/' do
-    'Hello world'
+    "Send a post request to shorten your URL"
+  end
+
+  post '/' do
+    req = JSON.parse(request.body.read)
+    { :short_url => 'short_url', :url => req["url"] }.to_json
   end
 
   run! if app_file == $0
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,10 +4,24 @@ describe UrlShortener do
   include Rack::Test::Methods
 
   let (:app) { UrlShortener.new }
+
     context "GET /" do
       it "responds status 200 OK" do
         get "/"
         expect(last_response.status).to eq 200
       end
-  end
+
+      it "gets a message" do
+        get "/"
+        expect(last_response.body).to eq "Send a post request to shorten your URL"
+      end
+    end
+
+    context "POST /" do
+      # it "includes the original url" do
+      #   # data = '{"short_url":"short_url","url":"http://www.farmdrop.com"}'
+      #   post "/"
+      #   expect(last_response.body).to eq 'hello'
+      # end
+    end
 end
