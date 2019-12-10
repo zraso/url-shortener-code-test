f55f9c446d1d29095f5a03ee28bdaf59c72fb1e0
zraso <zaira.rasool@hotmail.co.uk>
2019-12-10 13:17:47 +0000

    Method generates random 5 character url string

--- lib/url_entry.rb
+++ lib/url_entry.rb
@@ -1,2 +1,8 @@
 class URLEntry
+
+  def short_url
+    charset = Array('a'..'z') + Array(1..9)
+    @short_url = Array.new(5) { charset.sample }.join
+  end
+
 end
--- spec/url_entry_spec.rb
+++ spec/url_entry_spec.rb
@@ -3,7 +3,15 @@ require 'url_entry'
 describe URLEntry do
 
   describe '#short_url' do
+    it 'generates a string with 5 characters' do
+      expect(subject.short_url.length).to eq 5
+    end
 
+    it 'each string is unique' do
+      short_url_1 = subject.short_url
+      short_url_2 = subject.short_url
+      expect(short_url_1).not_to equal short_url_2
+    end
   end
   
 end
